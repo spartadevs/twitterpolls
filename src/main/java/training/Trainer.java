514dcd5ff177433ea96a41d1b131fbf51b244d3b
Abhinav <abhinav.varma123@gmail.com>
2016-11-24 13:58:49 -0800

    yeswanth's changes

--- src/main/java/training/Trainer.java
+++ src/main/java/training/Trainer.java
@@ -1,52 +1,56 @@
 package training;
-// $example on$
-import org.apache.spark.ml.classification.NaiveBayes;
-import org.apache.spark.ml.classification.NaiveBayesModel;
-import org.apache.spark.ml.evaluation.MulticlassClassificationEvaluator;
-import org.apache.spark.sql.Dataset;
-import org.apache.spark.sql.Row;
-import org.apache.spark.sql.SparkSession;
-// $example off$
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.api.java.function.Function;
+import org.apache.spark.api.java.function.PairFunction;
+import org.apache.spark.mllib.classification.NaiveBayes;
+import org.apache.spark.mllib.classification.NaiveBayesModel;
+import org.apache.spark.mllib.regression.LabeledPoint;
+import org.apache.spark.mllib.util.MLUtils;
+import java.io.Serializable;
 
+import scala.Tuple2;
 
-public class Trainer {
+public class Trainer implements Serializable{
+	
+	private static final long serialVersionUID = 1L;
+	
+    private JavaRDD<LabeledPoint> getTrainingData(JavaSparkContext jsc) {
+        String path = "resources/training.1600000.processed.noemoticon.txt";
+        return MLUtils.loadLibSVMFile(jsc.sc(), path,-1).toJavaRDD();
+    }
 
     public void train() {
-        SparkSession spark = SparkSession
-                .builder().master("local")
-                .appName("JavaNaiveBayesExample")
-                .getOrCreate();
+        SparkConf conf = new SparkConf().setMaster("local").setAppName("My App");
+        JavaSparkContext jsc = new JavaSparkContext(conf);
+        JavaRDD<LabeledPoint>[] tmp = getTrainingData(jsc).randomSplit(new double[]{0.8, 0.2});
+        JavaRDD<LabeledPoint> training = tmp[0]; // training set
+        JavaRDD<LabeledPoint> test = tmp[1]; // test set
+        final NaiveBayesModel model = NaiveBayes.train(training.rdd(), 1.0);
+        JavaPairRDD<Double, Double> predictionAndLabel =
+                test.mapToPair(new PairFunction<LabeledPoint, Double, Double>() {
+                    private static final long serialVersionUID = 1L;
 
-        // $example on$
-        // Load training data
-        Dataset<Row> dataFrame =
-                spark.read().format("libsvm")
-                        .load("src/main/resources/sample_libsvm_data.txt");
-        // Split the data into train and test
-        Dataset<Row>[] splits = dataFrame.randomSplit(new double[]{0.6, 0.4}, 1234L);
-        Dataset<Row> train = splits[0];
-        Dataset<Row> test = splits[1];
+					@Override
+                    public Tuple2<Double, Double> call(LabeledPoint p) {
+                        return new Tuple2<>(model.predict(p.features()), p.label());
+                    }
+                });
+        	double accuracy = predictionAndLabel.filter(new Function<Tuple2<Double, Double>, Boolean>() {
+            private static final long serialVersionUID = 1L;
 
-        // create the trainer and set its parameters
-        NaiveBayes nb = new NaiveBayes();
+			@Override
+            public Boolean call(Tuple2<Double, Double> pl) {
+                return pl._1().equals(pl._2());
+            }
+        }).count() / (double) test.count();
 
-        // train the model
-        NaiveBayesModel model = nb.fit(train);
-
-        // Select example rows to display.
-        Dataset<Row> predictions = model.transform(test);
-        predictions.show();
-
-        // compute accuracy on the test set
-        MulticlassClassificationEvaluator evaluator = new MulticlassClassificationEvaluator()
-                .setLabelCol("label")
-                .setPredictionCol("prediction")
-                .setMetricName("accuracy");
-        double accuracy = evaluator.evaluate(predictions);
-        System.out.println("Test set accuracy = " + accuracy);
-        // $example off$
-
-        spark.stop();
+        System.out.println("accuracy:" + accuracy);
+        // Save and load model
+        //model.save(jsc.sc(), "target/tmp/myNaiveBayesModel");
+        //NaiveBayesModel sameModel = NaiveBayesModel.load(jsc.sc(), "target/tmp/myNaiveBayesModel");
     }
 
     public static void main(String[] args) {
